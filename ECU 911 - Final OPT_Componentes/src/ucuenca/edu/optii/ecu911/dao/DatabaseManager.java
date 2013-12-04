acc8818c329d312dcabe9c2411dcfdc4f3466f97
may_bip@hotmail.com <may_bip@hotmail.com@87278882-4a94-5add-f7e4-51ed7b03f757>
2013-12-04 12:46:37 +0000

    ULTIMOS ESFUERXOS xd

---
+++ ECU 911 - Final OPT_Componentes/src/ucuenca/edu/optii/ecu911/dao/DatabaseManager.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucuenca.edu.optii.ecu911.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+
+public class DatabaseManager {
+
+	private Connection connection;
+	private Savepoint svpt;
+
+		//crea la conexion
+	public DatabaseManager()
+	{//localhost la misma pc
+            //autocommit si esta en true, se almacena automaticamente cuando saco algo
+            //si esta en false, no guarda automaticamente, sirve para actualizar cambios cuando yo quiero, no simempre
+		try {
+                    String url = "jdbc:postgresql://localhost/baseEcu?user=postgres&password=postgrespass";
+                    connection = DriverManager.getConnection(url);
+                    connection.setAutoCommit(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+                //rollback borre todo lo que he hecho desde la ultima vez q se guardo, hasta un comit anterior
+	}
+
+	public void commit()
+	{
+		try
+		{
+			connection.commit();
+			svpt = connection.setSavepoint();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection()
+	{
+		return connection;
+	}
+    //en caso que necesite hacer conexiones paralelas
+	public void openConnectionTo(String url)
+	{
+		try
+		{			
+			connection = DriverManager.getConnection(url, "postgres", "clave");
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+        //borrar todo lo hecho
+	public void rollBack()
+	{
+		try
+		{
+			if (svpt != null)
+			{
+				connection.rollback(svpt);
+				connection.commit();
+			}
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+    
+}
