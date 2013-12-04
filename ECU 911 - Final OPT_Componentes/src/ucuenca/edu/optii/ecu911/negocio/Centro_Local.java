bab7511c152e45a66ba3d8af1c8ce6e1e3499645
cuenta4384 <cuenta4384@87278882-4a94-5add-f7e4-51ed7b03f757>
2013-12-04 12:22:29 +0000

    Creacioon Claces CentroEcu_observador 			Centro_Local 			Centro_nacional 			Centro_Zonal

---
+++ ECU 911 - Final OPT_Componentes/src/ucuenca/edu/optii/ecu911/negocio/Centro_Local.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ucuenca.edu.optii.ecu911.negocio;
+
+/**
+ *
+ * @author cuent
+ */
+public class Centro_Local {
+    
+}
