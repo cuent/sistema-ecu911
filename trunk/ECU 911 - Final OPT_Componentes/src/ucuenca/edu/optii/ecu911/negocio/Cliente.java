837209278eed0b6bf37d733aa9ffb31c7e6b6476
may_bip@hotmail.com <may_bip@hotmail.com@87278882-4a94-5add-f7e4-51ed7b03f757>
2013-12-20 03:33:54 +0000

    ingresos funcionando

--- trunk/ECU 911 - Final OPT_Componentes/src/ucuenca/edu/optii/ecu911/negocio/Cliente.java
+++ trunk/ECU 911 - Final OPT_Componentes/src/ucuenca/edu/optii/ecu911/negocio/Cliente.java
@@ -4,11 +4,59 @@
  */
 package ucuenca.edu.optii.ecu911.negocio;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import ucuenca.edu.optii.ecu911.dao.ClienteDB;
+
 /**
  *
  * @author May
  */
 public class Cliente extends Persona{
+    Telefono mifono;
+
+    public Telefono getMifono() {
+        return mifono;
+    }
+
+    public void setMifono(Telefono mifono) {
+        this.mifono = mifono;
+    }
+    
     public void llamar(){
+    }
+    
+    
+      public boolean grabar() {
+        try {
+            ClienteDB unrol = new ClienteDB();
+            unrol.grabar(this);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+           JOptionPane.showMessageDialog(null, ex.getMessage());
+           return false;
+        }  
+    }
+      
+       public boolean buscar() {
+        try {
+            Cliente aux;
+            ClienteDB aspectoDB = new ClienteDB();
+            aux = aspectoDB.buscarTelefono(this.mifono.id);
+            
+            if(aux!=null){
+             setCedula(aux.getCedula());
+             setNombres(aux.getNombres());   
+             return true;
+            }else{       
+                 return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 }
