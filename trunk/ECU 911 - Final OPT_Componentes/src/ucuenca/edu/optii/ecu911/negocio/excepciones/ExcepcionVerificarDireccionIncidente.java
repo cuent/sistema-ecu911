9ff706f1cdd7e5b647ee8502c9e334e35f77c374
cuenta4384 <cuenta4384@87278882-4a94-5add-f7e4-51ed7b03f757>
2014-01-07 14:00:24 +0000

    Excepciones Alarmas

---
+++ trunk/ECU 911 - Final OPT_Componentes/src/ucuenca/edu/optii/ecu911/negocio/excepciones/ExcepcionVerificarDireccionIncidente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ucuenca.edu.optii.ecu911.negocio.excepciones;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cuent
+ */
+public class ExcepcionVerificarDireccionIncidente extends RuntimeException{
+
+    public ExcepcionVerificarDireccionIncidente(JFrame frame) {
+                JOptionPane.showMessageDialog(frame, "Ingrese la Direccion del Incidente");
+    }
+    
+}
