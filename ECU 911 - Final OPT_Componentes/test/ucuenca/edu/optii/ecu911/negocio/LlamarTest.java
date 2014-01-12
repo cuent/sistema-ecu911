2e1397b2da8347d24444e5af36ad6951d8dd38ce
cuenta4384 <cuenta4384@87278882-4a94-5add-f7e4-51ed7b03f757>
2014-01-12 01:54:11 +0000

    Pruebas Unitaras LlamarTest

---
+++ ECU 911 - Final OPT_Componentes/test/ucuenca/edu/optii/ecu911/negocio/LlamarTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucuenca.edu.optii.ecu911.negocio;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author cuent
+ */
+public class LlamarTest {
+
+    private Llamar llamar = null;
+    private ArrayList expResult = new ArrayList();
+
+    @Before
+    public void setUp() {
+        ArrayList l = new ArrayList();
+        l.add("Bomberos");
+
+        expResult.add("Centro ECU 911 Nacional");
+        expResult.add("Bomberos");
+        llamar = new Llamar(l, "nacional");
+    }
+
+    @After
+    public void tearDown() {
+        llamar = null;
+    }
+
+    /**
+     * Test of alertar method, of class Llamar.
+     */
+    @Test
+    public void testAlertar() {
+        System.out.println("alertar");
+        ArrayList result = llamar.alertar();
+
+        assertEquals("Las pruebas para una entidad bomberos en un Ecu Nacional",expResult, result);
+    }
+
+}
