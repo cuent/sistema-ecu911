22d4bcdf39c7cd84807c696f3c3d9a60ac16aed6
cuenta4384 <cuenta4384@87278882-4a94-5add-f7e4-51ed7b03f757>
2014-01-17 15:59:39 +0000

    XML Sirve y correcgido errores de la capa de Negocio

--- ECU 911 - Final OPT_Componentes/test/ucuenca/edu/optii/ecu911/negocio/CentroEcu_ObservadoTest.java
+++ ECU 911 - Final OPT_Componentes/test/ucuenca/edu/optii/ecu911/negocio/CentroEcu_ObservadoTest.java
@@ -1,8 +1,6 @@
 package ucuenca.edu.optii.ecu911.negocio;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
